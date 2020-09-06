049dddce0724f227c5a4da2d4f1b21d534a0453a
Kgford <automatedtestsolutions@gmail.com>
2020-09-06 16:53:16 -0400

    settings.py

--- tcli/settings.py
+++ tcli/settings.py
@@ -50,7 +50,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['tcli-inventory-management.herokuapp.com']
 
 
 # Application definition
